643e9645037cf6e9fb8dbec2523497208bc40411
larscom <larskniep@gmail.com>
2025-09-13 23:32:51 +0200

    add txt file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+use std::{collections::HashSet, fs};
+
 use crate::lexer::Lexer;
 
 mod lexer;
 
-fn main() {
-    let content = "Hello world! This is clean 123 content.";
+fn main() -> Result<(), std::io::Error> {
+    let mut unique_words = HashSet::new();
+
+    let content = fs::read_to_string("./pg84.txt")?;
+    // let content = "Hello World, Chapter 24";
+
     let content_chars = content.chars().collect::<Vec<char>>();
 
     for t in Lexer::new(&content_chars) {
-        println!("content={:?}", t)
+        println!("content={:?}", t);
+        unique_words.insert(t);
     }
+
+    println!("unique words: {}", unique_words.len());
+
+    Ok(())
 }
