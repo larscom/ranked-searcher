79895a2e121c2bbbea6dd86ae827a04e8ee847a4
larscom <larskniep@gmail.com>
2025-09-14 16:29:18 +0200

    add stuff

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,52 @@
-use std::{collections::HashSet, fs};
+use std::{collections::HashSet, fs, path::PathBuf};
 
 use crate::lexer::Lexer;
 
 mod lexer;
 
+struct Document {
+    path: PathBuf,
+    word_count: usize,
+}
+
 fn main() -> Result<(), std::io::Error> {
     let mut unique_words = HashSet::new();
 
-    let content = fs::read_to_string("./pg84.txt")?;
-    // let content = "Hello World, Chapter 24";
+    let dir = fs::read_dir("./test_data")?;
 
-    let content_chars = content.chars().collect::<Vec<char>>();
+    for file in dir {
+        let file_path = file?.path();
 
-    for t in Lexer::new(&content_chars) {
-        println!("content={:?}", t);
-        unique_words.insert(t);
+        let dot_file = file_path
+            .file_name()
+            .and_then(|s| s.to_str())
+            .map(|s| s.starts_with("."))
+            .unwrap_or(false);
+
+        if dot_file {
+            continue;
+        }
+
+        let file_content = fs::read_to_string(file_path);
+
+        if let Ok(content) = file_content {
+            let chars = content.chars().collect::<Vec<char>>();
+
+            for t in Lexer::new(&chars) {
+                unique_words.insert(t);
+            }
+        }
     }
 
-    println!("unique words: {}", unique_words.len());
+    println!("unqiue words: {}", unique_words.len());
 
     Ok(())
+}
+
+fn compute_tf(word_freq: f32, word_count: f32) -> f32 {
+    word_freq / word_count
+}
+
+fn compute_idf(total_docs: f32, word_count_docs: f32) -> f32 {
+    (total_docs / word_count_docs).log10()
 }
